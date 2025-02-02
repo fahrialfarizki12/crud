7008461acbe11b592bf1bc9d59b2e1717401d9c3
fahrialfarizki <fahrialfarizki858@gmail.com>
2025-02-02 17:30:00 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App.jsx";
-import HOME from "./HOME.jsx";
-import About from "./about.jsx";
+
+
 import Edit from "./layouts/Edit.jsx";
-import Table from "./components/Table.jsx";
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
